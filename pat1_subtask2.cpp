ea89879d9682a8f55b93eb11c8467d01bcd6f583
Tusile09 <tusilemavie1@gmail.com>
2026-04-20 11:59:35 +0200

    Update pat1_subtask2.cpp

--- pat1_subtask2.cpp
+++ pat1_subtask2.cpp
@@ -3,3 +3,10 @@ using namespace std;
 
 int main() {
     int temp1, temp2, temp3;
+    // Input first temperature
+    cout << "Enter first temperature reading: ";
+    cin >> temp1;
+
+    // Input second temperature
+    cout << "Enter second temperature reading: ";
+    cin >> temp2;

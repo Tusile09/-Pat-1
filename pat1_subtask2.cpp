9e99ddee489bb653f30440f0dabccbee40f1bc7d
Tusile09 <tusilemavie1@gmail.com>
2026-04-20 12:00:28 +0200

    Update pat1_subtask2.cpp

--- pat1_subtask2.cpp
+++ pat1_subtask2.cpp
@@ -10,3 +10,10 @@ int main() {
     // Input second temperature
     cout << "Enter second temperature reading: ";
     cin >> temp2;
+ // Check difference between first and second reading
+    if (temp2 - temp1 > 50) {
+        cout << "Reduce fryer heat before taking the third reading." << endl;
+    } 
+    else if (temp2 - temp1 < 10) {
+        cout << "Increase the fryer heat before taking the third reading." << endl;
+    }

1095487fd98ec7c80560b6e92b9c733b26c974cb
Tusile09 <tusilemavie1@gmail.com>
2026-04-20 12:01:24 +0200

    Update pat1_subtask2.cpp

--- pat1_subtask2.cpp
+++ pat1_subtask2.cpp
@@ -17,3 +17,16 @@ int main() {
     else if (temp2 - temp1 < 10) {
         cout << "Increase the fryer heat before taking the third reading." << endl;
     }
+   // Input third temperature
+    cout << "Enter third temperature reading: ";
+    cin >> temp3;
+
+    // Final check for frying
+    if (temp3 >= 150 && temp3 <= 190) {
+        cout << "You may start frying the Magwinyas." << endl;
+    } else {
+        cout << "Oil is not ready for frying!" << endl;
+    }
+
+    return 0;
+}
